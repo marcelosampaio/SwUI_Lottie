f601e44d6e61f238357e64e3ca313e87fabcb22c
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-07-08 11:52:08 -0300

    Initial Commit

---
+++ SwUI_Lottie/SwUI_LottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUI_LottieApp.swift
+//  SwUI_Lottie
+//
+//  Created by Marcelo Sampaio on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUI_LottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
